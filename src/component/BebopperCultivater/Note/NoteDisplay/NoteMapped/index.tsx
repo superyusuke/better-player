90719552b5996df8a1b6248af06fab9a9be9d991
Nakanishi, Yusuke <nakanishiyusuke0000@gmail.com>
2020-09-15 11:20:50 +0900

    スタイル修正

--- src/component/BebopperCultivater/Note/NoteDisplay/NoteMapped/index.tsx
+++ src/component/BebopperCultivater/Note/NoteDisplay/NoteMapped/index.tsx
@@ -4,6 +4,14 @@ import { View, StyleSheet, Text } from "react-native";
 import { convertNoteMetaToMappedNote } from "src/model/music/convert/toNoteMapped/convertNoteMetaToMappedNote";
 import { NoteMeta, Key } from "src/model/music/base";
 
+const styles = StyleSheet.create({
+  wrapper: {
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: 20,
+  },
+});
+
 type Props = {
   noteMeta: NoteMeta | null;
   musicKey?: Key;
@@ -13,13 +21,13 @@ export const NoteMapped = (props: Props) => {
   const { noteMeta, musicKey } = props;
 
   if (!musicKey || !noteMeta) {
-    return null;
+    return <View style={styles.wrapper} />;
   }
 
   const note = convertNoteMetaToMappedNote({ key: musicKey, noteMeta });
 
   return (
-    <View>
+    <View style={styles.wrapper}>
       <Text>{note.note}</Text>
     </View>
   );
