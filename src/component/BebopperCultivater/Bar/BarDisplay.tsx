f8f1cf12aa70a497543fbe3e8a695c3567b2330e
Nakanishi, Yusuke <nakanishiyusuke0000@gmail.com>
2020-09-15 09:13:42 +0900

    不要

--- src/component/BebopperCultivater/Bar/BarDisplay.tsx
+++ src/component/BebopperCultivater/Bar/BarDisplay.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { View, StyleSheet, TouchableOpacity, Text } from "react-native";
-import RNPickerSelect from "react-native-picker-select";
 import { Note } from "src/component/BebopperCultivater/Note";
 import { useContextHook } from "src/component/BebopperCultivater";
 
